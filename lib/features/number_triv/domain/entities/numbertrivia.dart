885926049894dd83166d27098bc850dbc4ecc31f
Rohit Tiwari <rohittiwari@KTSPL-M-63.local>
2024-06-08 12:51:34 +0530

    Use case init

---
+++ lib/features/number_triv/domain/entities/numbertrivia.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:equatable/equatable.dart';
+
+class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  NumberTrivia({
+    required this.text,
+    required this.number,
+  });
+
+  @override
+  List<Object?> get props => [text, number];
+}
+ 
